9b7b2fa98c31b79695eb82d19c9aa09488e47487
TurtleXYZ <mikkelzuuu@gmail.com>
2016-08-07 21:23:55 +0200

    Inital commit part 2

---
+++ MyIVDatabase/DeletePoke.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace MyIVDatabase
+{
+    public class DeletePoke
+    {
+        public static async void DeleteRow(string name)
+        {
+            SQLiteConnection cnc = new SQLiteConnection(Connection.ConnectString());
+
+            cnc.Open();
+
+            SQLiteCommand cmd = new SQLiteCommand(Queries.DeletePoke(name), cnc);
+
+            await cmd.ExecuteNonQueryAsync();
+
+            cnc.Close();
+        }
+    }
+}
